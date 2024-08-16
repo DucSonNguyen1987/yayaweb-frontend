fe15d77df77be4d279b08503a14cb8e2f840df14
Etienne Boutin <etienne.boutin@gmail.com>
2024-08-16 21:02:49 +0200

    ProductVignette fix link to Product Page (/product/)

--- components/ProductVignette.js
+++ components/ProductVignette.js
@@ -6,7 +6,7 @@ import styles from '../styles/ProductVignette.module.css';
 function ProductVignette(props) {
   return (
     <div className={styles.vignette}>
-      <Link href={`/products/${props.id}`}>
+      <Link href={`/product/${props.id}`}>
         <img src={props.imgSrc} alt={props.name} className={styles.productImage} />
       </Link>
       <h3 className={styles.productName}>{props.name}</h3>
@@ -17,7 +17,7 @@ function ProductVignette(props) {
       >
         Ajouter au panier
       </button>
-      <Link href={`/products/${props.id}`}>
+      <Link href={`/product/${props.id}`}>
         <a className={styles.productLink}>Voir le détail</a>
       </Link>
     </div>
