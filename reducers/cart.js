d24859b845e76f7514ed5d33e5095773b3ce7943
Etienne Boutin <etienne.boutin@gmail.com>
2024-08-17 11:30:04 +0200

    Cart reducer : remove console logs

--- reducers/cart.js
+++ reducers/cart.js
@@ -13,7 +13,6 @@ export const cartSlice = createSlice({
     initialState,
     reducers: {
         addToCart : (state, action) => {
-            console.log(action.payload);
             const foundIndex = state.value.items.findIndex(item => item.product.productId === action.payload.product.productId && item.product.options.volume.capacity === action.payload.product.options.volume.capacity);
             if(foundIndex !== -1) {
                 state.value.items[foundIndex].quantity += action.payload.quantity
@@ -23,12 +22,7 @@ export const cartSlice = createSlice({
             state.value.total = state.value.items.reduce((acc, item) => { return acc + ((item.product.price+item.product.options.volume.price) * item.quantity) }, 0);
         },
         removeFromCart : (state, action) => {
-            console.log('removeFromCart', action.payload.productId, action.payload.options.volume.capacity);
             const items = state.value.items;
-            console.log('items', items);
-            console.log('removeFromCart2', state.value.items.filter(item => {
-                return item.product.productId !== action.payload.productId && item.product.options.volume.capacity !== action.payload.options.volume.capacity;
-            }));
             state.value.items = state.value.items.filter(item => item.product.productId !== action.payload.productId || item.product.options.volume.capacity !== action.payload.options.volume.capacity);
             state.value.total = state.value.items.reduce((acc, item) => { return acc + ((item.product.price+item.product.options.volume.price) * item.quantity) }, 0);
         },
