f9b537bf88cf13e7da97902e84261063409b1ce0
Etienne Boutin <etienne.boutin@gmail.com>
2024-08-28 19:58:22 +0200

    User Orders Page : create page and display history of orders via Order components

--- components/Order/OrderDelivery.js
+++ components/Order/OrderDelivery.js
@@ -6,41 +6,52 @@ import { useSelector } from 'react-redux';
 function OrderDelivery(props) {
   const user = useSelector((state) => state.user.value);
 
+  console.log(props.order);
+  const deliveryAddress = props.order ? props.order.deliveryAddress : {
+    civility: user.gender === 'male' ? 'M.' : user.gender === 'female' ? 'Mme' : user.gender,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    streetNumber: user.address[0].streetNumber,
+    streetName: user.address[0].streetName,
+    zipCode: user.address[0].zipCode,
+    city: user.address[0].city,
+  };
+
   return (
     <div className={styles.orderDelivery}>
       <h2>Livraison</h2>
       <h3>Créneau de livraison</h3>
       <p>...</p>
       <h3>Adresse de livraison</h3>
-      {user.address && (
+      {deliveryAddress && (
         <div className={styles.userDeliveryAddress}>
           <div className={styles.userCivility}>
             <span className={styles.userLabel}>Civilité</span>
-            <span className={styles.userField}>{user.gender === 'male' ? 'M.' : user.gender === 'female' ? 'Mme' : user.gender}</span>
+            <span className={styles.userField}>{deliveryAddress.civility}</span>
           </div>
           <div className={styles.userFirstName}>
             <span className={styles.userLabel}>Prénom</span>
-            <span className={styles.userField}>{user.firstName}</span>
+            <span className={styles.userField}>{deliveryAddress.firstName}</span>
           </div>
           <div className={styles.userLastName}>
             <span className={styles.userLabel}>Nom</span>
-            <span className={styles.userField}>{user.lastName}</span>
+            <span className={styles.userField}>{deliveryAddress.lastName}</span>
           </div>
           <div className={styles.userStreetNumber}>
             <span className={styles.userLabel}>N°</span>
-            <span className={styles.userField}>{user.address[0].streetNumber}</span>
+            <span className={styles.userField}>{deliveryAddress.streetNumber}</span>
           </div>
           <div className={styles.userStreetName}>
             <span className={styles.userLabel}>Voie (rue, boulevard...)</span>
-            <span className={styles.userField}>{user.address[0].streetName}</span>
+            <span className={styles.userField}>{deliveryAddress.streetName}</span>
           </div>
           <div className={styles.userZipCode}>
             <span className={styles.userLabel}>Code Postal</span>
-            <span className={styles.userField}>{user.address[0].zipCode}</span>
+            <span className={styles.userField}>{deliveryAddress.zipCode}</span>
           </div>
           <div className={styles.userCity}>
             <span className={styles.userLabel}>Ville</span>
-            <span className={styles.userField}>{user.address[0].city}</span>
+            <span className={styles.userField}>{deliveryAddress.city}</span>
           </div>
         </div>
       )}
---
+++ pages/orders.js
@@ -0,0 +1,7 @@
+import UserOrders from '../components/UserOrders';
+
+function OrdersPage() {
+  return <UserOrders />;
+}
+
+export default OrdersPage;
