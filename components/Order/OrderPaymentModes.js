fb5d1abb679488126826c4d648babe0a80aafe1b
Duc Son Nguyen <duc.son.nguyen48@gmail.com>
2024-08-31 10:10:47 +0200

    stripe bug

--- components/Order/OrderPaymentModes.js
+++ components/Order/OrderPaymentModes.js
@@ -11,6 +11,9 @@ import { loadStripe} from '@stripe/stripe-js';
 const stripePromise= loadStripe(`${process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}`);
 
 
+console.log("stripePromise",stripePromise)
+
+
 function OrderPaymentModes(props) {
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart.value);
