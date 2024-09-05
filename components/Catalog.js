b4430e71e61ce636ccdef9330f5acbe7a1f77c50
Duc Son Nguyen <duc.son.nguyen48@gmail.com>
2024-09-06 01:47:34 +0200

    homepage not done yet

--- components/Catalog.js
+++ components/Catalog.js
@@ -19,7 +19,9 @@ function Catalog() {
       {categories.map((category, index) => (
         <div key={index} className={styles.range}>
           <h2>{category}</h2>
-          <img src="/icons/logo.png" alt={category} />
+          <div className={styles.cadre}>
+          <img className={styles.image} src={`/Bottles/${category}.png`} alt={category} />
+          </div>
           <Link href={`/products/${category.toLowerCase().replace(/\s+/g, '-')}`}>
             <button className={styles.viewProductsButton}>Voir les produits</button>
           </Link>
